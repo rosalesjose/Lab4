3f0302e19fc1c87339986d9cf24173deadf5033e
Jose <jrosales0001@gmail.com>
2018-01-24 19:01:28 -0500

    Recursive

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -8,29 +8,66 @@ namespace Lab4
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            //Input
-            int FirstNum;
-            FirstNum = ReadUserInput("Enter a number to find the factorial.");
+            bool RunProgram = true;
+            while (RunProgram)
+            {
+                int FirstNum;
+                FirstNum = ReadUserInput("Enter a number to find the factorial.");
 
-            Console.WriteLine("Factorial of {0} is {1}", FirstNum, GetFactorial(FirstNum));
+                //For Loop
+                Console.WriteLine("For Loop: Factorial of {0} is {1}", FirstNum, GetFactorial(FirstNum));
+
+                //Recursive
+                Console.WriteLine("Recursive: Factorial of {0} is {1}", FirstNum, Recursive(FirstNum));
+
+                Console.WriteLine("\nRun again (Y/N)?");
+                char DoAgain = char.Parse(Console.ReadLine());
+
+                if (DoAgain == 'y' || DoAgain == 'Y')
+                {
+                    Console.Clear();
+                }
+                else
+                {
+                    RunProgram = false;
+                }
+            }
         }
+
         public static int ReadUserInput(string UserPrompt)
         {
             Console.WriteLine(UserPrompt);
             int Input = int.Parse(Console.ReadLine());
             return Input;
         }
-        public static long GetFactorial (int FirstNum)
+
+        public static long GetFactorial(int FirstNum)
         {
-            long FactorialNum = 1;
-            for (int i = 1; i <= FirstNum; i++)
+            if (FirstNum == 0)
             {
-                FactorialNum = FactorialNum * i;                                
+                return 1;
             }
-            Console.WriteLine("Number entered: {0}", FactorialNum);
+
+            long FactorialNum = 1;
+
+            for (long i = 1; i <= FirstNum; i++)
+            {
+                FactorialNum = FactorialNum * i;
+            }
+             
             return FactorialNum;
+        }
+
+        public static long Recursive (int FirstNum)
+        {
+            if (FirstNum == 0)
+            {
+                return 1;
+            }
+            return FirstNum * Recursive(FirstNum - 1);
         }
     }
 }
